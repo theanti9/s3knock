c834c7ffb76acf38375e058ae069ef1d4c16c796
Ryan Merl <the.anti.9@gmail.com>
2015-12-18 10:30:26 -0800

    Fixed readme. Better python. Fixed file leak.

--- s3knock.py
+++ s3knock.py
@@ -1,14 +1,14 @@
 import urllib2,cookielib
 import random
+import signal
+import sys
 from sys import argv
 from bs4 import BeautifulSoup
 
-wordlist = argv[1]
-base = str(argv[2])
-position = argv[3]
-
-lines = [line.rstrip('\n') for line in open(wordlist)]
-length = len(lines)
+USAGE = """Usage:
+python s3knock.py wordlist term position
+Example: python s3knock.py wordlist.txt tumblr 1
+"""
 
 class bcolors:
     public = '\033[92m'
@@ -16,39 +16,79 @@ class bcolors:
     problem = '\033[91m'
     stop = '\033[0m'
 
-for i in range(0, length):
-    if position == '1':
-        site = "http://" + base + lines[i] + ".s3.amazonaws.com/"
-    else:
-        site = "https://" + lines[i] + base + ".s3.amazonaws.com/"
+discovered = []
 
-    hdr1 = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-          'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-          'Accept-Encoding': 'none',
-          'Accept-Language': 'en-US,en;q=0.8',
-          'Connection': 'keep-alive'}
-    hdr2 = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.11 Chrome/23.0.1271.64 Safari/537.16',
-          'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-          'Accept-Charset': 'utf-8;q=0.7,*;q=0.3',
-          'Accept-Encoding': 'none',
-          'Accept-Language': 'en-US,en;q=0.8',
-          'Connection': 'keep-alive'}
+def main(wordlist, base, position):
+    with open(wordlist) as wordlist_file:
+        lines = [line.rstrip('\n') for line in wordlist_file]
+    
+    for line in lines:
+        if position == 1:
+            site = "http://%s%s.s3.amazonaws.com/" %  (base, line)
+        else:
+            site = "https://%s%s.s3.amazonaws.com/" % (line, base)
+    
+        hdr1 = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'}
+        hdr2 = {'User-Agent': 'Mozilla/5.0 AppleWebKit/537.11 Chrome/23.0.1271.64 Safari/537.16',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Charset': 'utf-8;q=0.7,*;q=0.3',
+            'Accept-Encoding': 'none',
+            'Accept-Language': 'en-US,en;q=0.8',
+            'Connection': 'keep-alive'}
+    
+    
+        req = urllib2.Request(site, headers=random.choice([hdr1, hdr2]))
+    
+        try:
+            page = urllib2.urlopen(req)
+            xml = e.fp.read()
+            # soup = BeautifulSoup(xml, features='xml')
+            print bcolors.public + '[*] found : ' + site + " Public! " + bcolors.stop
+            discovered.append(site)
+        except urllib2.HTTPError, e:
+            xml = e.fp.read()
+            soup = BeautifulSoup(xml, features='xml')
+            for q in soup.find_all('Error'):
+                if q.find('Code').text != 'NoSuchBucket':
+                    print bcolors.exists + '[*] found : ' + site + " " + q.find('Code').text + bcolors.stop
+        except urllib2.URLError, e:
+            print 'INFO: Invalid domain format. No DNS resolution.'
+    
+    print_summary()
+    
+def print_summary():
+    print ""
+    if not discovered:
+        print "No public sites found!"
+        return
 
+    print "Summary of public sites found: "
+    for s in discovered:
+        print s
 
-    req = urllib2.Request(site, headers=random.choice([hdr1, hdr2]))
+def signal_handler(signal, frame):
+    print "\nCtrl+C detected. Exiting..."
+    print_summary()
+    sys.exit(0)
 
+if __name__ == '__main__':
+    if len(argv) < 4:
+        print "ERROR: Not enough arguments given"
+        print USAGE
+        sys.exit(1)
+    
+    wordlist = argv[1]
+    base = argv[2]
+    
     try:
-        page = urllib2.urlopen(req)
-        xml = e.fp.read()
-        soup = BeautifulSoup(xml, features='xml')
-        print bcolors.public + '[*] found : ' + site + " Public! " + bcolors.stop
-    except urllib2.HTTPError, e:
-        xml = e.fp.read()
-        soup = BeautifulSoup(xml, features='xml')
-        for q in soup.find_all('Error'):
-            if q.find('Code').text != 'NoSuchBucket':
-                print bcolors.exists + '[*] found : ' + site + " " + q.find('Code').text + bcolors.stop
-    except urllib2.URLError, e:
-        print 'INFO: Invalid domain format. No DNS resolution.'
-
+        position = int(argv[3])
+    except ValueError as e:
+        print "ERROR: position argument not a number"
+        sys.exit(1)
+    signal.signal(signal.SIGINT, signal_handler)
+    main(wordlist, base, position)
